b2524ff9a6afdabec06ceff0e975cff4687ba5dd
mortezaKia <morteza.kia8700@gmail.com>
2024-06-11 14:20:19 +0330

    "Updated TodoCard component: modified checkbox styling and text wrapping behavior"

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -15,15 +15,15 @@ export default function TodoCard({
   return (
     <div className="flex justify-between p-1 group bg-white border border-slate-500 rounded-lg shadow hover:bg-gray-100">
       <div className="flex flex-col w-4/5">
-        <div className="flex gap-2 items-center w-full">
+        <div className="flex gap-2 w-full">
           <input
             type="checkbox"
             value=""
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            className="w-4 h-4 mt-[0.5px] text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           />
-          <p className="whitespace-nowrap group-hover:text-wrap group-hover:whitespace-normal group-hover:overflow-visible overflow-hidden overflow-ellipsis">
+          <div className="truncate group-hover:overflow-visible">
             {text}
-          </p>
+          </div>
         </div>
         {todo.editMode ? (
           <div className=" text-red-700 flex items-center gap-2">
