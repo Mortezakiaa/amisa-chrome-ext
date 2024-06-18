8669950f194827be3d51f981d73d27bfd23ec669
mohammad molavi <mohamadmolavi98@gmail.com>
2024-06-18 10:35:40 +0430

    create Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+
+
+function Input() {
+  return (
+    <div>
+      <input type="text" color="blue" placeholder="متن مورد نظر خود را بنویسید..." className="my-5 w-1/4 p-2 rounded-lg   focus:outline-none border-2 border-slate-400  " />
+    </div>
+  );
+}
+
+export default Input;
