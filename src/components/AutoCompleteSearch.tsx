e3d36a8fc5d5af4a5e5a84c1bb5f0c7051775da1
mortezaKia <morteza.kia8700@gmail.com>
2024-06-23 15:15:40 +0330

    "Added AutoCompleteSearch component to Main component and updated grid layout"

---
+++ src/components/AutoCompleteSearch.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// src/components/Autocomplete.js
+import { useState } from "react";
+import Input from "./Input";
+import ApiService from "../utils/axios";
+
+function trailingDebounce(func: any, t: number) {
+  let timer: any;
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(args);
+    }, t);
+  };
+}
+
+const AutoCompleteSearch = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [filteredOptions, setFilteredOptions] = useState([]);
+
+  const search = async () => {
+    const data = await ApiService.get(inputValue);
+    console.log(data);
+  };
+
+  return (
+    <div className="relative w-full max-w-md mx-auto">
+      <Input
+        value={inputValue}
+        onchange={trailingDebounce(search, 800)}
+        placeholder="جستجو در گوگل"
+      />
+      {inputValue.length > 0 && (
+        <ul className="absolute z-10 w-full bg-white border border-gray-200 divide-y divide-gray-200 rounded-lg mt-1">
+          {filteredOptions.map((option, index) => (
+            <li
+              key={index}
+              className="px-4 py-3 cursor-pointer hover:bg-gray-100"
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default AutoCompleteSearch;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,16 @@
 import DateTimePicker from "./DateTimePicker";
+import AutoCompleteSearch from "./AutoCompleteSearch";
 import TodoList from "./TodoList";
 
 export default function Main() {
   return (
-    <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-4">
+    <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-5">
       <div className="h-screen bg-white rounded-lg col-span-1">
         <TodoList />
       </div>
-      <div className="h-screen col-span-1"></div>
+      <div className="h-screen col-span-2">
+        <AutoCompleteSearch/>
+      </div>
       <div className="flex flex-col h-screen col-span-1">
         <DateTimePicker />
       </div>
