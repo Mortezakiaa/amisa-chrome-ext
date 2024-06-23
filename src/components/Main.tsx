6041ec5f085097c3955648055ba7a487cdfc3791
mortezaKia <morteza.kia8700@gmail.com>
2024-06-23 16:55:42 +0330

    "Updated grid layout and column spans in Main component"

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,14 +4,14 @@ import TodoList from "./TodoList";
 
 export default function Main() {
   return (
-    <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-5">
-      <div className="h-screen bg-white rounded-lg col-span-1">
+    <div className="grid grid-cols-12 gap-5">
+      <div className="h-screen bg-white rounded-lg col-span-12 xl:col-span-3 md:col-span-6">
         <TodoList />
       </div>
-      <div className="h-screen col-span-2">
-        <AutoCompleteSearch/>
+      <div className="h-screen xl:col-span-5 col-span-12 md:col-span-6">
+        <AutoCompleteSearch />
       </div>
-      <div className="flex flex-col h-screen col-span-1">
+      <div className="flex flex-col h-screen xl:col-span-4 col-span-12 md:col-span-6">
         <DateTimePicker />
       </div>
     </div>
