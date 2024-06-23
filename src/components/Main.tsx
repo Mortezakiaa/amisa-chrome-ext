0ac689c3dd977637595a8688392266ef6b0bc3cd
mortezaKia <morteza.kia8700@gmail.com>
2024-06-23 09:52:46 +0330

    "Updated App.tsx with API key and CX, modified Input.tsx to use a floating label, and reformatted Main.tsx and index.css"

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ export default function Main() {
   return (
     <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-4">
       <div className="h-screen bg-white rounded-lg col-span-1">
-        <TodoList/>
+        <TodoList />
       </div>
       <div className="h-screen col-span-1"></div>
       <div className="flex flex-col h-screen col-span-1">
